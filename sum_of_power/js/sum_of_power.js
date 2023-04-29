224e2a57a710d4b5193616d860809d3bef73a79a
lahcenbcf <h.bencharef@esi-sba.dz>
2023-04-29 20:15:12 +0200

    sum_of_pow js

---
+++ sum_of_power/js/sum_of_power.js
@@ -0,0 +1,21 @@
+let array1 = [];
+let array2 = [];
+
+function findRacines(n) {
+  if (n == 0 || n == 1 || n == 2 || n == 3) {
+    return String(n);
+  } else {
+    let a = parseInt(Math.sqrt(n));
+    let b = n - Math.pow(a, 2);
+    array1.push(a);
+    findRacines(a);
+    
+      array2.push(b);
+      findRacines(b);
+    
+  }
+}
+
+findRacines(20)
+console.log(array1);
+console.log(array2);
