554e0d33d50068ecb9594a0fd72a63c28516a632
XmiTchiX <i.benhammadi@esi-sba.dz>
2023-04-29 17:57:33 +0200

    Finished caralort javascript

--- caralot/sol.js
+++ caralot/sol.js
@@ -1,11 +1,12 @@
-const solve = (n, events) => {
-  if (n % 2 == 1) return [-1, [-1]] //wahed dkhal w makhrejch
+const solve = (n, eventsss) => {
+  if (parseInt(n) % 2 == 1) return [-1, [-1]] //wahed dkhal w makhrejch
   let stack = [] // people entering and leaving in the current day
   let day = [] // number of people in the office in the current day
   let done = [] // people entered and left the current day
   let oute_index = [] // number of people who worked the current day * 2
   let oute = [] // data partition from the events list
 
+  const events = eventsss.split(' ').map((e) => parseInt(e))
   for (let i = 0; i < events.length; i++) {
     const e = events[i]
     if (done.includes(e)) return [-1, [-1]]
@@ -36,13 +37,8 @@ const solve = (n, events) => {
   return [oute.length, oute_index]
 }
 
-
 let n = '6'
-let events = '2 5 -5 5 -5 -2'
+let events = '2 5 -5 6 -6 -2'
 
-const sol = solve(
-  parseInt(n),
-  events.split(' ').map((e) => parseInt(e))
-)
+const sol = solve(n, events)
 console.log(`${sol[0]}\n${sol[1].join(' ')}`)
-
