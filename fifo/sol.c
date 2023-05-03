7ac802a207f43b9dff84a7f70c8f8282d3e384f0
ricin9 <ricin9@outlook.com>
2023-05-03 13:28:42 +0100

    fixed chall

--- fifo/sol.c
+++ fifo/sol.c
@@ -1,15 +1,8 @@
-#include<stdio.h>
-/*
-example input :
-1000
-100 200 300 400 500
-output :
-900
-3
-*/
+#include <stdio.h>
+
 int main()
 {
-    // read target
+    // read capacity
     int max_capacity;
     scanf("%d", &max_capacity);
 
@@ -17,23 +10,32 @@ int main()
     int arr[32];
     int last_free_index = 0;
     int num;
-    while (scanf("%d", &num) == 1) {
-        arr[last_free_index] = num;
-        last_free_index++;
+    char c;
+
+    fflush(stdin);
+    for (int i = 0; i < 32; i++)
+    {
+        int flag = scanf("%d%c", &arr[i], &c);
+        if (c == '\n' || flag == EOF) {
+            last_free_index = i + 1;
+            break;
+        };
     }
 
     // solution
     int current_capacity = 0;
     int oldest_item_index = 0;
-    for (int i=0; i < last_free_index; i++) {
-        while(current_capacity + arr[i] > max_capacity) {
+    for (int i = 0; i < last_free_index; i++)
+    {
+        while (current_capacity + arr[i] > max_capacity)
+        {
             current_capacity = current_capacity - arr[oldest_item_index];
             oldest_item_index++;
         }
 
         current_capacity += arr[i];
     }
-    
+
     printf("%d\n", current_capacity);
     printf("%d\n", oldest_item_index);
     return 0;
