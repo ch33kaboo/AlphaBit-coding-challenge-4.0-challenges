7eb0862c58d855c52ca7ae80bb105360e77faf08
Mohamed Miloudi <ricin9@outlook.com>
2023-05-04 18:44:15 +0100

    added caralot python impl

---
+++ caralot/sol.py
@@ -0,0 +1,40 @@
+n = int(input())
+events = list(map(int, input().split()))
+
+
+def solve(n, events):
+    if len(events) % 2  == 1 : return -1 ,[-1] #wahed dkhal w makhrejch
+    stack =[]  # people entering and leaving in the current day 
+    day =[]   # number of people in the office in the current day
+    done=[]   # people entered and left the current  day 
+    oute_index = [] # nmubr of people who worked the current day * 2
+    oute=[]   # dats partition from the events list
+    
+    for e in events:
+        if e not in done :
+            if e > 0 and e not in day: # entering 
+                day.append(e)
+                stack.append(e)
+            elif (e < 0) and (-e in day):  #departure
+                day.remove(abs(e))
+                stack.append(e)
+                done.append(abs(e))
+                if len(day)==0:  
+                    oute.append(stack.copy())
+                    del done[:]
+                    del day[:]
+                    del stack[:]
+            else : 
+                return -1 ,[-1]
+
+
+    oute_index = [len(num) for num in oute]
+
+
+    return len(oute), oute_index
+
+
+n , sol = solve(n, events)
+
+print(n)
+print(' '.join(map(str, sol)))
