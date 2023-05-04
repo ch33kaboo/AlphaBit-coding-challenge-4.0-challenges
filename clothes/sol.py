1c0b690429cf8d2c3b2371f426db58e122ea65a9
Mohamed Miloudi <ricin9@outlook.com>
2023-05-04 19:19:09 +0100

    fixed clothes/py switched naive solution to optimized

--- clothes/sol.py
+++ clothes/sol.py
@@ -1,25 +1,24 @@
-def naiive_sollution(input_file):
+def optimized_Sollution(input_file):
     n, m = map(int, input().split())
-    if (n < 3) or ( m < 3) : return -1 
+    if (n < 3) or (m<3): return -1 
     prices = list(map(int, input().split()))
-    matches = [set() for _ in range(n)]
+    matching_costs = [[float('inf') for _ in range(n)] for _ in range(n)]
     for _ in range(m):
-        u, v = map(int, input().split())
-        matches[u-1].add(v-1)
-        matches[v-1].add(u-1)
+        i, j = map(int, input().split())
+        matching_costs[i-1][j-1] = matching_costs[j-1][i-1] = min(prices[i-1], prices[j-1])
 
-    #Parsing 
+        
     min_cost = float('inf')
     for i in range(n):
-        for j in matches[i]:
-            for k in matches[i].intersection(matches[j]):
-                    cost = prices[i] + prices[j] + prices[k]
-                    if cost < min_cost:
-                        min_cost = cost
-
+        for j in range(i+1, n):
+            if matching_costs[i][j] == float('inf'):
+                continue
+            for k in range(j+1, n):
+                if matching_costs[i][k] != float('inf') and matching_costs[j][k] != float('inf'):
+                    min_cost = min(min_cost, prices[i]+prices[j]+prices[k])
 
     if min_cost == float('inf'):
-            return -1
+        return -1
     else:
-            return str(min_cost) 
-print(naiive_sollution('input.txt'))
+        return min_cost
+print(optimized_Sollution("input.txt"))
