14c06243c9ff687ce16179309b0079cc5a888429
Sayah Mahfoud Abdel Ali <73748467+M4hf0d@users.noreply.github.com>
2023-05-12 19:21:31 +0200

    Update twoSum.py

--- two_sum/twoSum.py
+++ two_sum/twoSum.py
@@ -1,16 +1,16 @@
 def two_sum(nums, target):
     lookup = {}
-    pairs = []
     for i, num in enumerate(nums):
         complement = target - num
         if complement in lookup:
-            pairs.append([lookup[complement], i])
+            return [lookup[complement], i]
         lookup[num] = i
-    return pairs
-
+    return []
 
 
 target = int(input())
 arr  = list(map(int, input().split()))
+print(' '.join(map(str, two_sum(arr, target))))
 
-print(two_sum(arr, target))
+
+
