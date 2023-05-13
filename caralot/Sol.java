130ae7b5b1038cd6c000f44452a9d259699e107d
ALLAOUA Okba <o.allaoua@esi-sba.dz>
2023-05-13 01:00:33 +0100

    Update Sol.java

--- caralot/Sol.java
+++ caralot/Sol.java
@@ -1,75 +1,61 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Sol {
-    static class Input {
-        int capacity;
-        int[] weights;
-        int[] uses;
-
-        public Input(int capacity, int[] weights, int[] uses) {
-            this.capacity = capacity;
-            this.weights = weights;
-            this.uses = uses;
-        }
-    }
-
-    static int sol(Input input) {
-        int currentCapacity = 0;
-        int[] frequencyMap = new int[6];
-        Arrays.fill(frequencyMap, 0);
-
-        for (int i = 0; i < input.uses.length; i++) {
-            int itemId = input.uses[i];
-
-            if (frequencyMap[itemId] == 0) {
-                while (currentCapacity + input.weights[itemId] > input.capacity) {
-                    int minItemId = 0;
-                    int minFrequency = Integer.MAX_VALUE;
-
-                    for (int j = 0; j < frequencyMap.length; j++) {
-                        if (frequencyMap[j] != 0 && frequencyMap[j] < minFrequency) {
-                            minItemId = j;
-                            minFrequency = frequencyMap[j];
-                        }
-                    }
-
-                    frequencyMap[minItemId] = 0;
-                    currentCapacity -= input.weights[minItemId];
-                }
-
-                currentCapacity += input.weights[itemId];
-            }
-
-            frequencyMap[itemId]++;
-        }
-
-        return currentCapacity;
-    }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter the capacity: ");
-        int capacity = scanner.nextInt();
-
-        System.out.print("Enter the weights (comma-separated white spaces): ");
-        String[] weightStrings = scanner.next().split(",");
-        int[] weights = new int[weightStrings.length];
-        for (int i = 0; i < weightStrings.length; i++) {
-            weights[i] = Integer.parseInt(weightStrings[i]);
+        int n = scanner.nextInt();
+        List<Integer> events = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            events.add(scanner.nextInt());
         }
 
-        System.out.print("Enter the uses (comma-separated no white spaces): ");
-        String[] useStrings = scanner.next().split(",");
-        int[] uses = new int[useStrings.length];
-        for (int i = 0; i < useStrings.length; i++) {
-            uses[i] = Integer.parseInt(useStrings[i]);
+        int[] result = solve(n, events);
+        System.out.println(result[0]);
+        for (int i = 1; i <= result[0]; i++) {
+            System.out.print(result[i] + " ");
+        }
+    }
+
+    public static int[] solve(int n, List<Integer> events) {
+        if (events.size() % 2 == 1) {
+            return new int[]{-1, -1};
         }
 
-        scanner.close();
+        List<Integer> stack = new ArrayList<>();
+        List<Integer> day = new ArrayList<>();
+        List<Integer> done = new ArrayList<>();
+        List<List<Integer>> oute = new ArrayList<>();
 
-        Input input = new Input(capacity, weights, uses);
-        System.out.println(sol(input)); // expected output: 800
+        for (Integer e : events) {
+            if (!done.contains(e)) {
+                if (e > 0 && !day.contains(e)) {
+                    day.add(e);
+                    stack.add(e);
+                } else if (e < 0 && day.contains(-e)) {
+                    day.remove(Integer.valueOf(-e));
+                    stack.add(e);
+                    done.add(-e);
+                    if (day.size() == 0) {
+                        oute.add(new ArrayList<>(stack));
+                        done.clear();
+                        day.clear();
+                        stack.clear();
+                    }
+                } else {
+                    return new int[]{-1, -1};
+                }
+            }
+        }
+
+        int[] outeIndex = new int[oute.size() + 1];
+        outeIndex[0] = oute.size();
+        for (int i = 1; i <= oute.size(); i++) {
+            outeIndex[i] = oute.get(i - 1).size();
+        }
+
+        return outeIndex;
     }
 }
